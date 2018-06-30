e9e550d5fa6367b50f8289970ec12d5a0ad1498f
marylene.chan@gmail.com <marylene.chan@gmail.com>
2018-06-30 04:18:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-    str = "Fizz" # Go fizz
+    "Fizz" # Go fizz
   elsif int % 5 == 0
-    str = "Buzz"
+    "Buzz"
   elsif int % 15 == 0
-    str = "FizzBuzz"
-  else str = nil  
+    "FizzBuzz"
+  else nil  
   end
 end
  
